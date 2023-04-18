d33bdec633214e5fd81cf249992895a31267921f
RalphKK05 <131179349+RalphKK05@users.noreply.github.com>
2023-04-18 17:48:58 +0300

    Create getpaintingprice.php

---
+++ getpaintingprice.php
@@ -0,0 +1,21 @@
+<?php
+
+$conn = mysqli_connect("localhost", "id20511553_ralphadmin", "+/}2mVFCirUSGT~K", "id20511553_testingdatabase");
+
+
+$sql_query = "SELECT * FROM Painting WHERE price >= $minPrice AND price <= $maxPrice;";
+
+$result = mysqli_query($conn, $sql_query);
+
+if(mysqli_num_rows($result) > 0 ){
+
+$row = mysqli_fetch_assoc($result);
+$my_integer = $row["userID"];
+echo $my_integer;
+
+
+}
+mysqli_close($conn);
+
+
+?>
